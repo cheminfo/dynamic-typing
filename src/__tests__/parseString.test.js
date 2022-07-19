466fccbfcc1e7d90d5681c03d9c87a5c154c84d3
Luc Patiny <luc@patiny.com>
2022-07-19 07:09:43 +0200

    chore: update dependencies

--- src/__tests__/parseString.test.js
+++ src/__tests__/parseString.test.js
@@ -1,18 +1,18 @@
 import { parseString } from '../parseString';
 
 test('parseString', () => {
-  expect(parseString('')).toStrictEqual('');
-  expect(parseString(' ')).toStrictEqual(' ');
-  expect(parseString('.')).toStrictEqual('.');
-  expect(parseString('0.')).toStrictEqual(0);
-  expect(parseString('0.0')).toStrictEqual(0);
-  expect(parseString('abc')).toStrictEqual('abc');
-  expect(parseString('True')).toStrictEqual(true);
-  expect(parseString('false')).toStrictEqual(false);
-  expect(parseString('0')).toStrictEqual(0);
-  expect(parseString('123')).toStrictEqual(123);
-  expect(parseString('123.456')).toStrictEqual(123.456);
-  expect(parseString('12e3')).toStrictEqual(12000);
-  expect(parseString('0x10')).toStrictEqual(16);
-  expect(parseString('0b10')).toStrictEqual(2);
+  expect(parseString('')).toBe('');
+  expect(parseString(' ')).toBe(' ');
+  expect(parseString('.')).toBe('.');
+  expect(parseString('0.')).toBe(0);
+  expect(parseString('0.0')).toBe(0);
+  expect(parseString('abc')).toBe('abc');
+  expect(parseString('True')).toBe(true);
+  expect(parseString('false')).toBe(false);
+  expect(parseString('0')).toBe(0);
+  expect(parseString('123')).toBe(123);
+  expect(parseString('123.456')).toBe(123.456);
+  expect(parseString('12e3')).toBe(12000);
+  expect(parseString('0x10')).toBe(16);
+  expect(parseString('0b10')).toBe(2);
 });
