23e5f7ff488f432f41e7c31ac15febec66bc257a
Wadjih Bencheikh <jw_bencheikh@esi.dz>
2024-03-24 09:23:08 +0100

    refactor: setup project for TypeScript (#9)

--- src/parseString.ts
+++ src/parseString.ts
@@ -1,16 +1,11 @@
-/**
- * Dynamically type a string
- * @param {string} value String to dynamically type
- * @returns {boolean|string|number}
- */
-export function parseString(value) {
+export function parseString(value: string): boolean | string | number {
   if (value.length === 4 || value.length === 5) {
-    let lowercase = value.toLowerCase();
+    const lowercase = value.toLowerCase();
 
     if (lowercase === 'true') return true;
     if (lowercase === 'false') return false;
   }
-  let number = Number(value);
+  const number = Number(value);
   if (number === 0 && !value.includes('0')) {
     return value;
   }
